a6b35e4a0ab85c85aa7c31d95bb8786727405daf
Lasha Efremidze <efremidzel@hotmail.com>
2017-04-06 22:28:49 -0700

    backup

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -9,34 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-//    @IBOutlet weak var button: UIButton! {
-//        didSet {
-//            
-//        }
-//    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func tapped(_ button: UIButton) {
-        Haptic.selection.generate()
+        button.isHaptic = true
+        if button.isHaptic {
+            print("--->")
+        }
+//        button.isHaptic = false
+//        if button.isHaptic {
+//            print("--->")
+//        }
     }
     
 }
-
-//protocol Haptic {
-//    var isHaptic: Bool { get set }
-//}
-//
-//extension UIButton: Haptic {}
 
 enum Haptic {
     case impact(UIImpactFeedbackStyle)
@@ -60,28 +45,36 @@ enum Haptic {
             generator.selectionChanged()
         }
     }
-    
-//    func handle(state: UIGestureRecognizerState) {
-//        switch state {
-//        case .began:
-//            feedbackGenerator = UISelectionFeedbackGenerator()
-//            feedbackGenerator?.prepare()
-//        case .changed:
-//            feedbackGenerator?.selectionChanged()
-//            feedbackGenerator?.prepare()
-//        case .cancelled, .ended, .failed:
-//            feedbackGenerator = nil
-//        default:
-//            break
-//        }
-//    }
 }
 
-//private extension UIFeedbackGenerator {
-//    static func impactFeedbackGenerator(style: UIImpactFeedbackStyle) -> UIImpactFeedbackGenerator {
-//        return impactFeedbackGenerators[style.rawValue]
-//    }
-//    private static let impactFeedbackGenerators = [UIImpactFeedbackGenerator(style: .light), UIImpactFeedbackGenerator(style: .medium), UIImpactFeedbackGenerator(style: .heavy)]
-//    static let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
-//    static let selectionFeedbackGenerator = UISelectionFeedbackGenerator()
-//}
+protocol Hapticable {
+    var isHaptic: Bool { get set }
+}
+
+extension UIButton: Hapticable {
+    var isHaptic: Bool {
+        get {
+            for target in allTargets {
+                if let actions = actions(forTarget: target, forControlEvent: .touchDown) {
+                    for action in actions {
+                        if action == #selector(trigger).description {
+                            return true
+                        }
+                    }
+                }
+            }
+            return false
+        }
+        set {
+            if newValue {
+                addTarget(self, action: #selector(trigger), for: .touchDown)
+            } else {
+                removeTarget(self, action: #selector(trigger), for: .touchDown)
+            }
+        }
+    }
+    
+    func trigger(_ button: UIButton) {
+        Haptic.impact(.heavy).generate()
+    }
+}
