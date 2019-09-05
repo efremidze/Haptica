95294124da430067c02f20a45705b5ef0ede0c95
Rafael Francisco <rafaelfrancisco6@gmail.com>
2019-09-05 15:38:56 +0100

    Fixed SwiftPM manifest

--- Package.swift
+++ Package.swift
@@ -1,3 +1,14 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let package = Package(name: "Haptica")
+let package = Package(
+    name: "Haptica",
+    products: [
+        .library(name: "Haptica",  targets: ["Haptica"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Haptica", path: "Sources")
+    ]
+)
