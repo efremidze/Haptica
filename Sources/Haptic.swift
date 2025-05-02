34d37c74999dde42a9a94431a92dd5aefed04968
Lasha Efremidze <efremidzel@hotmail.com>
2025-05-02 00:47:32 -0700

    Update Haptic.swift

--- Sources/Haptic.swift
+++ Sources/Haptic.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+/// Describes impact feedback styles with varying intensity.
 public enum HapticFeedbackStyle: Int {
     case light, medium, heavy
     case soft, rigid
@@ -19,6 +20,7 @@ extension HapticFeedbackStyle {
     }
 }
 
+/// Describes notification feedback types for task outcomes.
 public enum HapticFeedbackType: Int {
     case success, warning, error
 }
@@ -29,26 +31,54 @@ extension HapticFeedbackType {
     }
 }
 
+/// Primary interface for generating haptic feedback in Haptica.
 public enum Haptic {
     case impact(HapticFeedbackStyle)
     case notification(HapticFeedbackType)
     case selection
     
-    // trigger
+    // Semantic variants
+    case start
+    case stop
+    case increase
+    case decrease
+    case success
+    case failure
+    case warning
+    
+    /// Triggers the haptic feedback corresponding to the case.
     public func generate() {
         switch self {
         case .impact(let style):
             let generator = UIImpactFeedbackGenerator(style: style.value)
             generator.prepare()
             generator.impactOccurred()
+            
         case .notification(let type):
             let generator = UINotificationFeedbackGenerator()
             generator.prepare()
             generator.notificationOccurred(type.value)
+            
         case .selection:
             let generator = UISelectionFeedbackGenerator()
             generator.prepare()
             generator.selectionChanged()
+            
+            // Semantic mappings
+        case .start:
+            Haptic.impact(.medium).generate()
+        case .stop:
+            Haptic.impact(.rigid).generate()
+        case .increase:
+            Haptic.impact(.light).generate()
+        case .decrease:
+            Haptic.impact(.soft).generate()
+        case .success:
+            Haptic.notification(.success).generate()
+        case .failure:
+            Haptic.notification(.error).generate()
+        case .warning:
+            Haptic.notification(.warning).generate()
         }
     }
 }
